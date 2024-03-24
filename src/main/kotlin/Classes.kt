277333515f6f148e9f0629ab45883687c8d781fe
AchroDev <ztblaine@protonmail.com>
2024-03-23 23:48:24 -0500

    Adding a "turnOn" and "turnOff" property(function) to the "SmartDevice" Class. All of this is initialized is the "main" function.

--- src/main/kotlin/Classes.kt
+++ src/main/kotlin/Classes.kt
@@ -1,9 +1,17 @@
 // Define a class
 
 class SmartDevice {
+    fun turnOn() {
+        println("Smart device has been powered on.")
+    }
 
+    fun turnOff() {
+        println("Smart device has been powered off.")
+    }
 }
 
 fun main() {
     val smartTvDevice = SmartDevice()
+    smartTvDevice.turnOn()
+    smartTvDevice.turnOff()
 }
