ca4cef83855dbed4ad322cc733fad3a639169061
AchroDev <ztblaine@protonmail.com>
2024-03-26 21:42:18 -0500

    Finished the "Implement a relationship between classes" section of the Use classes and objects in Kotlin Codelab. I updated the application to have complete relations between the sub and superclasses. Lastly, adding a "deviceType" method to define the device even further.

--- src/main/kotlin/Classes.kt
+++ src/main/kotlin/Classes.kt
@@ -1,29 +1,164 @@
 // Define a class
 
-class SmartDevice(val name: String, val category: String) {
+// In Kotlin classes are final by default, you need to add "open" to be able to extend the class
+open class SmartDevice(val name: String, val category: String) {
 
+    // Defines the class property "deviceStatus" so that other devices will know if it is on or off.
     var deviceStatus = "online"
 
-    constructor(name: String, category: String, statusCode: Int) : this("Android TV", "Entertainment") {
-        deviceStatus = when (statusCode) {
-            0 -> "offline"
-            1 -> "online"
-            else -> "unknown"
+    // Defines the deviceType property of the SmartDevice class. Default is "unknown".
+    open val deviceType = "unknown"
+
+    // Defines class method to turn on the device.
+    open fun turnOn() {
+        deviceStatus = "on"
+    }
+
+    // Defines class method to turn off the device.
+    open fun turnOff() {
+        deviceStatus = "off"
+    }
+}
+
+/*
+deviceName and deviceCategory are specific to the subclasses and need to be defined as so within the -
+- constructor definitions/parameters. These are not considered class properties.
+*/
+// This subclass creates an IS-A inheritance relationship to the primary class SmartDevice
+class SmartTvDevice(deviceName: String, deviceCategory: String) : SmartDevice(name = deviceName, category = deviceCategory) {
+
+    // Defines the device type as "Smart TV" overriding the method from the superclass.
+    override val deviceType = "Smart TV"
+
+    // Sets the device volume property and function to specify the available range.
+    var speakerVolume = 2
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
         }
+
+    // Sets the device channel number property and function to specify the available range.
+    var channelNumber = 1
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    // Defines the method to increase the speaker volume and prints the current value.
+    fun increaseSpeakerVolume() {
+        speakerVolume++
+        println("Speaker volume increased to $speakerVolume")
     }
 
-    fun turnOn() {
-        println("Smart device has been powered on.")
+    // Defines the method to increase the channel number and prints the current value.
+    fun nextChannel() {
+        channelNumber++
+        println("Channel number increased to $channelNumber")
     }
 
-    fun turnOff() {
-        println("Smart device has been powered off.")
+    override fun turnOn() {
+        super.turnOn()
+        println("$name is turned on. Speaker volume is set to $speakerVolume and channel number is " + "set to $channelNumber.")
+    }
+
+    override fun turnOff() {
+        super.turnOff()
+        println("$name turned off")
+    }
+}
+
+// Defining a subclass to create a smart light off of the SmartDevice primary class.
+class SmartLightDevice(deviceName: String, deviceCategory: String) : SmartDevice(name = deviceName, category = deviceCategory) {
+
+    // Defines the device type as "Smart Light" overriding the method from the superclass.
+    override val deviceType = "Smart Light"
+
+    // Defining the initial brightness for the light.
+    var brightnessLevel = 0
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    // Defining the method to increase the brightness.
+    fun increaseBrightness() {
+        brightnessLevel++
+        println("Brightness level increased to $brightnessLevel")
+    }
+
+    // Method to turn on the lights and assign properties. now uses the "super" keyword to pull from the superclass
+    override fun turnOn() {
+        super.turnOn()
+        brightnessLevel = 2
+        println("$name turned on. The brightness level is $brightnessLevel.")
+    }
+    
+    // Method to turn off the lights and assign properties. now uses the "super" keyword to pull from the superclass.
+    override fun turnOff() {
+        super.turnOff()
+        brightnessLevel = 0
+        println("$name turned off.")
+    }
+}
+
+// Defining a new "SmartHome" subclass that provides an example of the HAS-A relationship to the SmartTvDevice and light.
+class SmartHome(
+    val smartTvDevice: SmartTvDevice,
+    val smartLightDevice: SmartLightDevice
+) {
+
+    // Method to turn on the Smart TV.
+    fun turnOnTv() {
+        smartTvDevice.turnOn()
+    }
+
+    // Method to turn off the Smart TV.
+    fun turnOffTv() {
+        smartTvDevice.turnOff()
+    }
+
+    // Method to increase the Smart TV volume.
+    fun increaseTvVolume() {
+        smartTvDevice.increaseSpeakerVolume()
+    }
+
+    // Method to change to the next channel on the Smart TV.
+    fun changeTvChannelToNexT() {
+        smartTvDevice.nextChannel()
+    }
+
+    // Method to turn on the Smart Light.
+    fun turnOnLight() {
+        smartLightDevice.turnOn()
+    }
+
+    // Method to turn off the Smart Light.
+    fun turnOffLight() {
+        smartLightDevice.turnOff()
+    }
+
+    // Method to increase the Smart Lights brightness.
+    fun increaseLightBrightness() {
+        smartLightDevice.increaseBrightness()
+    }
+
+    // Method to turn off all Smart Devices.
+    fun turnOffAllDevices() {
+        turnOffTv()
+        turnOffLight()
     }
 }
 
 fun main() {
-    val smartTvDevice = SmartDevice(name = "", category = "", statusCode = 0)
-    println("Device name is: ${smartTvDevice.name}")
-    smartTvDevice.turnOn()
-    smartTvDevice.turnOff()
+
+    // Initializing the smartDevice variable to define a device from one of the classes and execute the code.
+    var smartDevice: SmartDevice = SmartTvDevice("Android TV", "Entertainment")
+    smartDevice.turnOn()
+
+    // Reassigning the smartDevice variable from a TV to a Light, providing an example of polymorphism.
+    smartDevice = SmartLightDevice("Google Light", "Utility")
+    smartDevice.turnOn()
 }
